1364423dcfe3fc5b0906fe68b0dec88503db477e
LorenzMacke <lorenz.macke@edu.fhdw.de>
2020-05-26 13:06:14 +0200

    Adds login

--- index.js
+++ index.js
@@ -1,62 +1,75 @@
-    //first add an event listener for page load
-    document.addEventListener("DOMContentLoaded", get_json_data, false); // get_json_data is the function name that will fire on page load
+//first add an event listener for page load
+document.addEventListener("DOMContentLoaded", check_for_auth, false);
 
-    //this function is in the event listener and will execute on page load
-    function get_json_data() {
-        // Relative URL of external json file
-        var json_url = '/rest/task';
-
-        //Build the XMLHttpRequest (aka AJAX Request)
-        xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) { //when a good response is given do this
-
-                    var data = JSON.parse(this.responseText); // convert the response to a json object
-                    append_json(data); // pass the json object to the append_json function
-                }
-            }
-            //set the request destination and type
-        xmlhttp.open("GET", json_url, true);
-        //set required headers for the request
-        xmlhttp.setRequestHeader("Content-type", "application/json");
-        // send the request
-        xmlhttp.send(); // when the request completes it will execute the code in onreadystatechange section
+function check_for_auth() {
+    if (getCookie("username") == "") {
+        return;
     }
 
-    //this function appends the json data to the table 'gable'
-    function append_json(data) {
-        var tableHTML = "<tr>";
-        for (var headers in data[0]) {
-            tableHTML += "<th>" + headers + "</th>";
+    get_json_data()
+}
+
+//this function is in the event listener and will execute on page load
+function get_json_data() {
+    fetch('/rest/task', {
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        headers: {
+            'Content-Type': 'application/json'
         }
+    }).then((response) => {
+        return response.json();
+    }).then((responseJson) => {
+        append_json(responseJson)
+    })
+}
+
+//this function appends the json data to the table 'gable'
+function append_json(data) {
+    var tableHTML = "<tr>";
+    for (var headers in data[0]) {
+        tableHTML += "<th>" + headers + "</th>";
+    }
+    tableHTML += "</tr>";
+    for (var eachItem in data) {
+        tableHTML += "<tr>";
+        var dataObj = data[eachItem];
+        for (var eachValue in dataObj) {
+            tableHTML += "<td>" + dataObj[eachValue] + "</td>";
+        }
+        tableHTML += "<td> <input class='button-claim' type='button' value='Claim Task' onclick='claim_task(\"" + dataObj["id"].toString() + "\")'/> </td>";
         tableHTML += "</tr>";
+    }
+    document.getElementById('tasklist').innerHTML = tableHTML;
+}
 
-        for (var eachItem in data) {
-            tableHTML += "<tr>";
-            var dataObj = data[eachItem];
-            for (var eachValue in dataObj) {
-                tableHTML += "<td>" + dataObj[eachValue] + "</td>";
-            }
-            tableHTML += "<td> <input class='button-claim' type='button' value='Claim Task' onclick='claim_task(\"" + dataObj["id"].toString() + "\")'/> </td>";
-            tableHTML += "</tr>";
+function claim_task(id) {
+    fetch('/rest/message', {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        headers: {
+            'Content-Type': 'application/json'
         }
+    }).then((response) => {
+        return response.json();
+    }).then((responseJson) => {
+        console.log("ProcessInstance object:", responseJson);
+        const processInstanceId = responseJson[0].processInstance.id;
+        console.log("ProcessInstanceId: ", processInstanceId);
+    })
+}
 
-        document.getElementById('tasklist').innerHTML = tableHTML;
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
-
-    function claim_task(id) {
-        fetch('/rest/message', {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(startConfig)
-        }).then((response) => {
-            return response.json();
-        }).then((responseJson) => {
-            console.log("ProcessInstance object:", responseJson);
-            const processInstanceId = responseJson[0].processInstance.id;
-            console.log("ProcessInstanceId: ", processInstanceId);
-        })
-    }
+    return "";
+}
---
+++ login.js
@@ -0,0 +1,31 @@
+function handleLogin() {
+    var user = document.login["username"].value;
+    var pw = document.login["password"].value;
+
+    if (user.length < 1 || pw.length < 1) {
+        alert("Username or password cannot be empty.")
+    }
+
+    fetch('/rest/identity/verify', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username: user, password: pw })
+    }).then((response) => {
+        if (!response.ok) {
+            alert("Something went wrong.");
+            return false;
+        }
+        return response.json();
+    }).then((responseJson) => {
+        console.log(responseJson);
+        if (responseJson.authenticated == false) {
+            alert("Invalid username/password. Try again.")
+        }
+        document.cookie = "username=" + responseJson.authenticatedUser
+        window.location.href = "index.html";
+    })
+    return false;
+}
